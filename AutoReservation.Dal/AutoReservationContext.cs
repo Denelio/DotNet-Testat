354a4324df6aa21aa37a91de1af57d061e72fa92
Denelio <denelio16@gmail.com>
2019-12-12 10:23:43 +0100

    Update Context

--- AutoReservation.Dal/AutoReservationContext.cs
+++ AutoReservation.Dal/AutoReservationContext.cs
@@ -1,8 +1,24 @@
-﻿namespace AutoReservation.Dal
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoReservation.Dal
 {
     public class AutoReservationContext
         : AutoReservationContextBase
     {
-      
+        public DbSet<Auto> Autos { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Reservation> Reservationen { get; set; }
+
+        AutoReservationContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Auto>().HasDiscriminator<int>("Autoklasse").HasValue<LuxusklasseAuto>(0).HasValue<MittelklasseAuto>(1).HasValue<StandardAuto>(2);
+        }
     }
 }
