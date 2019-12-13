80f6df76be8a99d20cfe5f4000473a019f3566b7
flObvious <floris.staubce@gmail.com>
2019-12-13 13:46:31 +0100

    added ReservationManager

--- AutoReservation.BusinessLayer.Testing/ReservationUpdateTest.cs
+++ AutoReservation.BusinessLayer.Testing/ReservationUpdateTest.cs
@@ -18,10 +18,15 @@ namespace AutoReservation.BusinessLayer.Testing
         [Fact]
         public async Task UpdateReservationTest()
         {
-            throw new NotImplementedException("Test not implemented.");
+            //throw new NotImplementedException("Test not implemented.");
             // arrange
             // act
             // assert
+
+            var result = await _target.GetById(2);
+            DateTime thisDate1 = new DateTime(2020, 1, 10);
+            var resultDate = (result.Bis - result.Von).TotalHours;
+            Assert.Equal(1, resultDate);
         }
     }
 }
--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -1,10 +1,77 @@
-﻿using System;
+﻿using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoReservation.BusinessLayer
 {
     public class ReservationManager
         : ManagerBase
     {
+        public async Task<List<Reservation>> GetAll()
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            return await context.Reservationen.ToListAsync();
+        }
+
+        public async Task<Reservation> GetById(int i)
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            return await context.Reservationen.SingleOrDefaultAsync(r => r.ReservationsNr == i);
+        }
+
+        public async Task<Reservation> Insert(Reservation reservation)
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            try
+            {
+                context.Entry(reservation).State = EntityState.Added;
+                await context.SaveChangesAsync();
+                return reservation;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw CreateOptimisticConcurrencyException(context, reservation);
+            }
+
+        }
+
+        public async Task<Reservation> Update(Reservation reservation)
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            try
+            {
+                context.Entry(reservation).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return reservation;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw CreateOptimisticConcurrencyException(context, reservation);
+            }
+        }
+
+        public async Task Delete(Reservation reservation)
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            try
+            {
+                context.Entry(reservation).State = EntityState.Deleted;
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw CreateOptimisticConcurrencyException(context, reservation);
+            }
+
+        }
+
+        public async Task<bool> isReservationValid(Reservation reservation)
+        {
+            return (reservation.Bis - reservation.Von).TotalHours < 24 && reservation.Bis < reservation.Von;
+        }
     }
 }
